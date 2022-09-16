81eba8b7c70d6c0d11e14c2b98af69f9ec5d15ca
MarceloVichar <marcos.marcelo43@gmail.com>
2022-09-16 11:00:13 -0300

    refatorando componentes estaticos

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
 
-void main() => runApp(const ExpensesApp());
+main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({super.key});
-
+  const ExpensesApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-    );
+    return const MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Despesas pessoais'),
+        title: const Text('Despesas Pessoais'),
       ),
-      body: const Center(child: Text('Versão inicial')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          SizedBox(
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Gráfico'),
+            ),
+          ),
+          TransactionUser(),
+        ],
+      ),
     );
   }
 }
